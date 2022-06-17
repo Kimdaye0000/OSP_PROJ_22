5856a544f18386d0bdbb26c7330fbe63c5fb669d
kimdaye0000 <daye0529@cu.ac.kr>
2022-06-17 13:11:13 +0900

    3rd commit : Variable Name

--- webcr.py
+++ webcr.py
@@ -4,31 +4,31 @@ country = '모로코'
 res = requests.get('https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query='+country+'+여행+준비')
 soup = BeautifulSoup(res.content, 'html.parser')
 
-mydata = soup.select("#nxTsOv div.item-2sM-R")#입국조건
-mydata1 = soup.select("#nxTsOv div.item-PxYX-")#한국귀국시
-mydata2 = soup.select("#nxTsOv div.cm_content_area li.item")#여행전체크하기
-mydata4 = soup.select("#nxTsOv div.item")#추천여행기간
+entry = soup.select("#nxTsOv div.item-2sM-R")#입국조건
+home = soup.select("#nxTsOv div.item-PxYX-")#한국귀국시
+check = soup.select("#nxTsOv div.cm_content_area li.item")#여행전체크하기
+recommend = soup.select("#nxTsOv div.item")#추천여행기간
 
 #입국조건
-for i in mydata:
-    clear_txt = i.get_text().split("여부")	    
-    print(f"{clear_txt[0]}여부 : {clear_txt[1]}  ")
+for i in entry:
+    entry_txt = i.get_text().split("여부")	    
+    print(f"{entry_txt[0]}여부 : {entry_txt[1]}  ")
 print("\n")
 
 #한국귀국시
-for k in mydata1:
-    clear1_txt = k.get_text()
-    print(clear1_txt)
+for k in home:
+    home_txt = k.get_text()
+    print(home_txt)
 print("\n")    
 
 #여행전체크하기
-for j in mydata2:
-    clear2_txt = j.get_text()
-    print(clear2_txt)
+for j in check:
+    check_txt = j.get_text()
+    print(check_txt)
 print("\n")    
 
 #추천여행기간   
-for m in mydata4:
-    clear4_txt = m.get_text()
-    print(clear4_txt)    
+for m in recommend:
+    recommend_txt = m.get_text()
+    print(recommend_txt)    
 	 
