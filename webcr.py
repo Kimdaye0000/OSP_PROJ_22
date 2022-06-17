6cd00ca2824943a14b677feb51447c05630b3b94
kimdaye0000 <daye0529@cu.ac.kr>
2022-06-17 13:37:47 +0900

    4th commit : Code Upload

--- webcr.py
+++ webcr.py
@@ -1,34 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
+
+#외교부해외안전여행_안전공지
+notice = requests.get('https://www.0404.go.kr/dev/newest_list.mofa')
+sp = BeautifulSoup(notice.content, 'html.parser')
+
+#제목
+title = sp.select("#content.Content h3.Ctit")
+
+#제목 출력
+for t in title:
+    title_txt = t.get_text()
+    print(title_txt)    
+print("\n")
+
+#여행 관련 정보
 country = '모로코'
 res = requests.get('https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query='+country+'+여행+준비')
 soup = BeautifulSoup(res.content, 'html.parser')
 
-entry = soup.select("#nxTsOv div.item-2sM-R")#입국조건
-home = soup.select("#nxTsOv div.item-PxYX-")#한국귀국시
-check = soup.select("#nxTsOv div.cm_content_area li.item")#여행전체크하기
-recommend = soup.select("#nxTsOv div.item")#추천여행기간
-
 #입국조건
-for i in entry:
-    entry_txt = i.get_text().split("여부")	    
+entry = soup.select("#nxTsOv div.item-2sM-R")
+#한국귀국시
+home = soup.select("#nxTsOv div.item-PxYX-")
+#여행전체크하기
+check = soup.select("#nxTsOv div.cm_content_area li.item")
+#추천여행기간
+recommend = soup.select("#nxTsOv div.item")
+
+#입국조건 출력
+for e in entry:
+    entry_txt = e.get_text().split("여부")	    
     print(f"{entry_txt[0]}여부 : {entry_txt[1]}  ")
 print("\n")
 
-#한국귀국시
-for k in home:
-    home_txt = k.get_text()
+#한국귀국시 출력
+for h in home:
+    home_txt = h.get_text()
     print(home_txt)
 print("\n")    
 
-#여행전체크하기
-for j in check:
-    check_txt = j.get_text()
+#여행전체크하기 출력
+for c in check:
+    check_txt = c.get_text()
     print(check_txt)
 print("\n")    
 
-#추천여행기간   
-for m in recommend:
-    recommend_txt = m.get_text()
+#추천여행기간 출력   
+for r in recommend:
+    recommend_txt = r.get_text()
     print(recommend_txt)    
 	 
