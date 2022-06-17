eceb274720898844b3eb835a1fb9d663e996bf6d
byungwoo0601 <rlaquddn0601@naver.com>
2022-06-17 15:14:47 +0900

    2th commit: Code Upload

--- app.py
+++ app.py
@@ -9,9 +9,14 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 @app.route("/")
-def web1():
+def index():
 	return redirect("https://jfk3ow.axshare.com/#id=so8yho&p=main")
 	
+@app.reute('/info', methods=['GET'])
+def info:
+	if request.method == 'GET':
+		myname = request.args.get('name')
+		return render_template('info.thml', name=myname)
 
 if __name__ == '__main__':
 	app.run(debug=True)
