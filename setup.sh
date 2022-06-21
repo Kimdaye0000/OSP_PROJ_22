f358cbd3f632579d0b233e3b12ee9aef3e6b537a
nanocode00 <rwg0901@naver.com>
2022-06-21 23:32:50 +0900

    4th commit: added execute commands

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# root diretory로 이동
-cd
-
 # 파이썬 실행환경 체크
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 
@@ -10,24 +7,26 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 pip install requests beautifulsoup4 elasticsearch
 
 # elasticsearch 확인 후 설치
-ES_PATH="elasticsearch-8.2.0"
-if [ ! -d $ES_PATH ]; then
+ES_PATH="~/elasticsearch-8.2.0"
+if [ ! -d ~/$ES_PATH ]; then
     # elasticsearch 다운로드
-    ES_GZ="elasticsearch-8.2.0-linux-x86_64.tar.gz"
-    if [ ! -f $ES_GZ ]; then
+    ES_GZ="~/elasticsearch-8.2.0-linux-x86_64.tar.gz"
+    if [ ! -f ~/$ES_GZ ]; then
         wget https://artifacts.elastic.co/downloads/elasticsearch/$ES_GZ
     fi
-    tar xvzf $ES_GZ
+    tar xvzf ~/$ES_GZ
 
     # elasticsearch 설정
-    $ES_PATH/bin/elasticsearch -d -p es.pid
-    while [ ! -f $ES_PATH/es.pid ]
+    ~/$ES_PATH/bin/elasticsearch -d -p es.pid
+    while [ ! -f ~/$ES_PATH/es.pid ]
     do
         sleep 1
     done
-    read < $ES_PATH/es.pid PID
+    read < ~/$ES_PATH/es.pid PID
     kill $PID
 fi
-sed -i 's/xpack.security.enabled: true/xpack.security.enabled: false/' $ES_PATH/config/elasticsearch.yml
+sed -i 's/xpack.security.enabled: true/xpack.security.enabled: false/' ~/$ES_PATH/config/elasticsearch.yml
 
-# (optional) 리눅스 부팅시 elasticsearch 자동 실행 설정
+# 스크립트 실행
+~/$ES_PATH/bin/elasticsearch -d -p es.pid
+python app.py
