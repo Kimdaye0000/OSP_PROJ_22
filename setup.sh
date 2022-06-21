e95e1788ad8d22e5f1ced6dc17e058976d67f5ff
nanocode00 <rwg0901@naver.com>
2022-06-21 22:12:16 +0900

    3th commit: elasticsearch setting

--- setup.sh
+++ setup.sh
@@ -1,16 +1,33 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# home 경로로 이동
+# root diretory로 이동
 cd
 
 # 파이썬 실행환경 체크
-update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 
 # 파이썬 모듈 설치 : requests, beautifulsoup4, elasticsearch
 pip install requests beautifulsoup4 elasticsearch
 
-# elasticsearch 다운로드
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.2.0-linux-x86_64.tar.gz
-tar xvzf elasticsearch-8.2.0-linux-x86_64.tar.gz
+# elasticsearch 확인 후 설치
+ES_PATH="elasticsearch-8.2.0"
+if [ ! -d $ES_PATH ]; then
+    # elasticsearch 다운로드
+    ES_GZ="elasticsearch-8.2.0-linux-x86_64.tar.gz"
+    if [ ! -f $ES_GZ ]; then
+        wget https://artifacts.elastic.co/downloads/elasticsearch/$ES_GZ
+    fi
+    tar xvzf $ES_GZ
+
+    # elasticsearch 설정
+    $ES_PATH/bin/elasticsearch -d -p es.pid
+    while [ ! -f $ES_PATH/es.pid ]
+    do
+        sleep 1
+    done
+    read < $ES_PATH/es.pid PID
+    kill $PID
+fi
+sed -i 's/xpack.security.enabled: true/xpack.security.enabled: false/' $ES_PATH/config/elasticsearch.yml
 
 # (optional) 리눅스 부팅시 elasticsearch 자동 실행 설정
